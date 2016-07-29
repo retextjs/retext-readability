302b79a2178003cb1ec3bc24e2c10375aeb3b388
Titus Wormer <tituswormer@gmail.com>
2016-07-29 18:24:34 +0200

    Refactor code-style to use `xo`

--- index.js
+++ index.js
@@ -8,14 +8,13 @@
 
 'use strict';
 
-/* eslint-env commonjs */
-
 /* Dependencies. */
-var daleChall = require('dale-chall/data/dale-chall.json');
-var spache = require('spache/data/spache.json');
+var has = require('has');
 var visit = require('unist-util-visit');
 var toString = require('nlcst-to-string');
 var syllable = require('syllable');
+var daleChall = require('dale-chall/data/dale-chall.json');
+var spache = require('spache/data/spache.json');
 var daleChallFormula = require('dale-chall-formula');
 var ari = require('automated-readability');
 var colemanLiau = require('coleman-liau');
@@ -24,17 +23,138 @@ var smog = require('smog-formula');
 var gunningFog = require('gunning-fog');
 var spacheFormula = require('spache-formula');
 
+/* Expose. */
+module.exports = attacher;
+
 /* Constants. */
+var SOURCE = 'retext-readability';
 var DEFAULT_TARGET_AGE = 16;
 var WORDYNESS_THRESHOLD = 5;
 var DEFAULT_THRESHOLD = 4 / 7;
 
 /* Methods. */
-var has = {}.hasOwnProperty;
 var floor = Math.floor;
 var round = Math.round;
 var ceil = Math.ceil;
 var sqrt = Math.sqrt;
+
+/**
+ * Attacher.
+ *
+ * @param {Retext} processor - Processor.
+ * @param {Object?} [options] - Configuration.
+ * @return {Function} - `transformer`.
+ */
+function attacher(processor, options) {
+  var settings = options || {};
+  var targetAge = settings.age || DEFAULT_TARGET_AGE;
+  var threshold = settings.threshold || DEFAULT_THRESHOLD;
+  var minWords = settings.minWords;
+
+  if (minWords === null || minWords === undefined) {
+    minWords = WORDYNESS_THRESHOLD;
+  }
+
+  return function (tree, file) {
+    visit(tree, 'SentenceNode', gather);
+
+    /**
+     * Gather a sentence.
+     *
+     * @param {Node} sentence - Logical grouping.
+     */
+    function gather(sentence) {
+      var familiarWords = {};
+      var easyWord = {};
+      var complexPolysillabicWord = 0;
+      var familiarWordCount = 0;
+      var polysillabicWord = 0;
+      var totalSyllables = 0;
+      var easyWordCount = 0;
+      var wordCount = 0;
+      var letters = 0;
+      var counts;
+      var caseless;
+
+      visit(sentence, 'WordNode', function (node) {
+        var syllables;
+        var value;
+        var head;
+
+        wordCount++;
+        value = toString(node);
+        head = value.charAt(0);
+        syllables = syllable(value);
+
+        totalSyllables += syllables;
+        letters += value.length;
+        caseless = value.toLowerCase();
+
+        /**
+         * Count complex words for gunning-fog based on
+         * whether they’re have three or more syllables
+         * and whether they are not proper nouns.  The
+         * last is checkt a little simple, so this
+         * index might be over-eager.
+         */
+
+        if (syllables >= 3) {
+          polysillabicWord++;
+
+          if (head !== head.toUpperCase()) {
+            complexPolysillabicWord++;
+          }
+        }
+
+        /* Find unique unfamiliar words for spache. */
+        if (
+          spache.indexOf(caseless) !== -1 &&
+          !has(familiarWords, caseless)
+        ) {
+          familiarWords[caseless] = true;
+          familiarWordCount++;
+        }
+
+        /* Find unique difficult words for dale-chall. */
+        if (
+          daleChall.indexOf(caseless) !== -1 &&
+          !has(easyWord, caseless)
+        ) {
+          easyWord[caseless] = true;
+          easyWordCount++;
+        }
+      });
+
+      if (wordCount < minWords) {
+        return;
+      }
+
+      counts = {
+        complexPolysillabicWord: complexPolysillabicWord,
+        polysillabicWord: polysillabicWord,
+        unfamiliarWord: wordCount - familiarWordCount,
+        difficultWord: wordCount - easyWordCount,
+        syllable: totalSyllables,
+        sentence: 1,
+        word: wordCount,
+        character: letters,
+        letter: letters
+      };
+
+      report(file, sentence, threshold, targetAge, [
+        gradeToAge(daleChallFormula.gradeLevel(
+          daleChallFormula(counts)
+        )[1]),
+        gradeToAge(ari(counts)),
+        gradeToAge(colemanLiau(counts)),
+        fleschToAge(flesch(counts)),
+        smogToAge(smog(counts)),
+        gradeToAge(gunningFog(counts)),
+        gradeToAge(spacheFormula(counts))
+      ]);
+    }
+  };
+}
 
 /**
  * Calculate the typical starting age when someone joins
@@ -47,7 +167,7 @@ var sqrt = Math.sqrt;
  *   of students in `grade` grade.
  */
 function gradeToAge(grade) {
-    return round(grade + 5);
+  return round(grade + 5);
 }
 
 /**
@@ -57,7 +177,7 @@ function gradeToAge(grade) {
  * @return {number} - Typical age for `value`.
  */
 function fleschToAge(value) {
-    return 20 - floor(value / 10);
+  return 20 - floor(value / 10);
 }
 
 /**
@@ -70,8 +190,10 @@ function fleschToAge(value) {
  * @return {number} - Typical age for `value`.
  */
 function smogToAge(value) {
-    return ceil(sqrt(value) + 2.5);
+  return ceil(sqrt(value) + 2.5);
 }
+
+/* eslint-disable max-params */
 
 /**
  * Report the `results` if they’re reliably too hard for
@@ -85,149 +207,28 @@ function smogToAge(value) {
  *   for `node` according to several algorithms.
  */
 function report(file, node, threshold, target, results) {
-    var length = results.length;
-    var index = -1;
-    var failCount = 0;
-    var message;
-    var confidence;
+  var length = results.length;
+  var index = -1;
+  var failCount = 0;
+  var message;
+  var confidence;
 
-    while (++index < length) {
-        if (results[index] > target) {
-            failCount++;
-        }
+  while (++index < length) {
+    if (results[index] > target) {
+      failCount++;
     }
+  }
 
-    if (failCount / results.length >= threshold) {
-        confidence = failCount + '/' + results.length;
+  if (failCount / results.length >= threshold) {
+    confidence = failCount + '/' + results.length;
 
-        message = file.warn(
-            'Hard to read sentence (confidence: ' + confidence + ')',
-            node,
-            'retext-readability'
-        );
+    message = file.warn(
+      'Hard to read sentence (confidence: ' + confidence + ')',
+      node,
+      SOURCE
+    );
 
-        message.confidence = confidence;
-        message.source = 'retext-readability';
-    }
+    message.confidence = confidence;
+    message.source = SOURCE;
+  }
 }
-
-/**
- * Attacher.
- *
- * @param {Retext} processor - Processor.
- * @param {Object?} [options] - Configuration.
- * @return {Function} - `transformer`.
- */
-function attacher(processor, options) {
-    var settings = options || {};
-    var targetAge = settings.age || DEFAULT_TARGET_AGE;
-    var threshold = settings.threshold || DEFAULT_THRESHOLD;
-    var minWords = settings.minWords;
-
-    if (minWords === null || minWords === undefined) {
-        minWords = WORDYNESS_THRESHOLD;
-    }
-
-    return function (tree, file) {
-        /**
-         * Gather a sentence.
-         *
-         * @param {Node} sentence - Logical grouping.
-         */
-        function gather(sentence) {
-            var familiarWords = {};
-            var easyWord = {};
-            var complexPolysillabicWord = 0;
-            var familiarWordCount = 0;
-            var polysillabicWord = 0;
-            var totalSyllables = 0;
-            var easyWordCount = 0;
-            var wordCount = 0;
-            var letters = 0;
-            var counts;
-            var caseless;
-
-            visit(sentence, 'WordNode', function (node) {
-                var syllables;
-                var value;
-                var head;
-
-                wordCount++;
-                value = toString(node);
-                head = value.charAt(0);
-                syllables = syllable(value);
-
-                totalSyllables += syllables;
-                letters += value.length;
-                caseless = value.toLowerCase();
-
-                /**
-                 * Count complex words for gunning-fog based on
-                 * whether they’re have three or more syllables
-                 * and whether they are not proper nouns.  The
-                 * last is checkt a little simple, so this
-                 * index might be over-eager.
-                 */
-
-                if (syllables >= 3) {
-                    polysillabicWord++;
-
-                    if (head !== head.toUpperCase()) {
-                        complexPolysillabicWord++;
-                    }
-                }
-
-                /* Find unique unfamiliar words for spache. */
-                if (
-                    spache.indexOf(caseless) !== -1 &&
-                    !has.call(familiarWords, caseless)
-                ) {
-                    familiarWords[caseless] = true;
-                    familiarWordCount++;
-                }
-
-                /* Find unique difficult words for dale-chall. */
-                if (
-                    daleChall.indexOf(caseless) !== -1 &&
-                    !has.call(easyWord, caseless)
-                ) {
-                    easyWord[caseless] = true;
-                    easyWordCount++;
-                }
-            });
-
-            if (wordCount < minWords) {
-                return;
-            }
-
-            counts = {
-                complexPolysillabicWord: complexPolysillabicWord,
-                polysillabicWord: polysillabicWord,
-                unfamiliarWord: wordCount - familiarWordCount,
-                difficultWord: wordCount - easyWordCount,
-                syllable: totalSyllables,
-                sentence: 1,
-                word: wordCount,
-                character: letters,
-                letter: letters
-            };
-
-            report(file, sentence, threshold, targetAge, [
-                gradeToAge(daleChallFormula.gradeLevel(
-                    daleChallFormula(counts)
-                )[1]),
-                gradeToAge(ari(counts)),
-                gradeToAge(colemanLiau(counts)),
-                fleschToAge(flesch(counts)),
-                smogToAge(smog(counts)),
-                gradeToAge(gunningFog(counts)),
-                gradeToAge(spacheFormula(counts))
-            ]);
-        }
-
-        visit(tree, 'SentenceNode', gather);
-    };
-}
-
-/* Expose. */
-module.exports = attacher;
--- test.js
+++ test.js
@@ -8,10 +8,6 @@
 
 'use strict';
 
-/* eslint-env node */
-/* jscs:disable jsDoc */
-/* jscs:disable maximumLineLength */
-
 /* Dependencies. */
 var test = require('tape');
 var retext = require('retext');
@@ -19,154 +15,144 @@ var readability = require('./');
 
 /* Tests. */
 test('readability', function (t) {
-    retext()
-        .use(readability)
-        .process('The cat sat on the mat', function (err, file) {
-            t.ifError(err, 'should not fail (#1)');
+  retext()
+    .use(readability)
+    .process('The cat sat on the mat', function (err, file) {
+      t.ifError(err, 'should not fail (#1)');
 
-            t.deepEqual(
-                file.messages.map(String),
-                [],
-                'should not warn when a sentence is easy to read'
-            );
-        });
+      t.deepEqual(
+          file.messages.map(String),
+          [],
+          'should not warn when a sentence is easy to read'
+      );
+    });
 
-    retext()
-        .use(readability)
-        .process([
-            'Oberon, also designated Uranus IV, is the outermost ',
-            'major moon of the planet Uranus and quite large',
-            'and massive for a Uranian moon.',
-            ''
-        ].join('\n'), function (err, file) {
-            t.ifError(err, 'should not fail (#2)');
+  retext()
+    .use(readability)
+    .process([
+      'Oberon, also designated Uranus IV, is the outermost ',
+      'major moon of the planet Uranus and quite large',
+      'and massive for a Uranian moon.',
+      ''
+    ].join('\n'), function (err, file) {
+      t.ifError(err, 'should not fail (#2)');
 
-            t.deepEqual(
-                file.messages.map(String),
-                ['1:1-3:32: Hard to read sentence (confidence: 4/7)'],
-                'should warn when low confidence that a sentence is hard to read'
-            );
-        });
+      t.deepEqual(
+        file.messages.map(String),
+        ['1:1-3:32: Hard to read sentence (confidence: 4/7)'],
+        'should warn when low confidence that a sentence is hard to read'
+      );
+    });
 
-    retext()
-        .use(readability, {
-            threshold: 5 / 7
-        })
-        .process([
-            'Oberon, also designated Uranus IV, is the outermost ',
-            'major moon of the planet Uranus and quite large',
-            'and massive for a Uranian moon.',
-            ''
-        ].join('\n'), function (err, file) {
-            t.ifError(err, 'should not fail (#3)');
+  retext()
+    .use(readability, {threshold: 5 / 7})
+    .process([
+      'Oberon, also designated Uranus IV, is the outermost ',
+      'major moon of the planet Uranus and quite large',
+      'and massive for a Uranian moon.',
+      ''
+    ].join('\n'), function (err, file) {
+      t.ifError(err, 'should not fail (#3)');
 
-            t.deepEqual(
-                file.messages.map(String),
-                [],
-                'should support a threshold'
-            );
-        });
+      t.deepEqual(
+        file.messages.map(String),
+        [],
+        'should support a threshold'
+      );
+    });
 
-    retext()
-        .use(readability, {
-            age: 18
-        })
-        .process([
-            'Oberon, also designated Uranus IV, is the outermost ',
-            'major moon of the planet Uranus and quite large',
-            'and massive for a Uranian moon.',
-            ''
-        ].join('\n'), function (err, file) {
-            t.ifError(err, 'should not fail (#4)');
+  retext()
+    .use(readability, {age: 18})
+    .process([
+      'Oberon, also designated Uranus IV, is the outermost ',
+      'major moon of the planet Uranus and quite large',
+      'and massive for a Uranian moon.',
+      ''
+    ].join('\n'), function (err, file) {
+      t.ifError(err, 'should not fail (#4)');
 
-            t.deepEqual(
-                file.messages.map(String),
-                [],
-                'should support a given age (removing the warning)'
-            );
-        });
+      t.deepEqual(
+        file.messages.map(String),
+        [],
+        'should support a given age (removing the warning)'
+      );
+    });
 
-    retext()
-        .use(readability, {
-            age: 14
-        })
-        .process([
-            'Oberon, also designated Uranus IV, is the outermost ',
-            'major moon of the planet Uranus and quite large',
-            'and massive for a Uranian moon.',
-            ''
-        ].join('\n'), function (err, file) {
-            t.ifError(err, 'should not fail (#5)');
+  retext()
+    .use(readability, {age: 14})
+    .process([
+      'Oberon, also designated Uranus IV, is the outermost ',
+      'major moon of the planet Uranus and quite large',
+      'and massive for a Uranian moon.',
+      ''
+    ].join('\n'), function (err, file) {
+      t.ifError(err, 'should not fail (#5)');
 
-            t.deepEqual(
-                file.messages.map(String),
-                ['1:1-3:32: Hard to read sentence (confidence: 5/7)'],
-                'should support a given age (upping the warning)'
-            );
-        });
+      t.deepEqual(
+        file.messages.map(String),
+        ['1:1-3:32: Hard to read sentence (confidence: 5/7)'],
+        'should support a given age (upping the warning)'
+      );
+    });
 
-    retext()
-        .use(readability)
-        .process([
-            'Oberon, also designated Uranus IV, is the outermost ',
-            'major moon of the planet Uranus and the second-largest ',
-            'and second most massive of the Uranian moons.',
-            ''
-        ].join('\n'), function (err, file) {
-            t.ifError(err, 'should not fail (#6)');
+  retext()
+    .use(readability)
+    .process([
+      'Oberon, also designated Uranus IV, is the outermost ',
+      'major moon of the planet Uranus and the second-largest ',
+      'and second most massive of the Uranian moons.',
+      ''
+    ].join('\n'), function (err, file) {
+      t.ifError(err, 'should not fail (#6)');
 
-            t.deepEqual(
-                file.messages.map(String),
-                ['1:1-3:46: Hard to read sentence (confidence: 5/7)'],
-                'should warn when moderately confident that a sentence is hard to read'
-            );
-        });
+      t.deepEqual(
+        file.messages.map(String),
+        ['1:1-3:46: Hard to read sentence (confidence: 5/7)'],
+        'should warn when moderately confident that a sentence is hard to read'
+      );
+    });
 
-    retext()
-        .use(readability, {
-            age: 14
-        })
-        .process([
-            'Oberon, also designated Uranus IV, is the outermost ',
-            'major moon of the planet Uranus and the second-largest ',
-            'and second most massive of the Uranian moons, and the ',
-            'ninth most massive moon in the Solar System.',
-            ''
-        ].join('\n'), function (err, file) {
-            t.ifError(err, 'should not fail (#7)');
+  retext()
+    .use(readability, {age: 14})
+    .process([
+      'Oberon, also designated Uranus IV, is the outermost ',
+      'major moon of the planet Uranus and the second-largest ',
+      'and second most massive of the Uranian moons, and the ',
+      'ninth most massive moon in the Solar System.',
+      ''
+    ].join('\n'), function (err, file) {
+      t.ifError(err, 'should not fail (#7)');
 
-            t.deepEqual(
-                file.messages.map(String),
-                ['1:1-4:45: Hard to read sentence (confidence: 6/7)'],
-                'should warn when highly confident that a sentence is hard to read'
-            );
-        });
+      t.deepEqual(
+        file.messages.map(String),
+        ['1:1-4:45: Hard to read sentence (confidence: 6/7)'],
+        'should warn when highly confident that a sentence is hard to read'
+      );
+    });
 
-    retext()
-        .use(readability)
-        .process('Honorificabilitudinitatibus.', function (err, file) {
-            t.ifError(err, 'should not fail (#8)');
+  retext()
+    .use(readability)
+    .process('Honorificabilitudinitatibus.', function (err, file) {
+      t.ifError(err, 'should not fail (#8)');
 
-            t.deepEqual(
-                file.messages.map(String),
-                [],
-                'should support minWords (default)'
-            );
-        });
+      t.deepEqual(
+        file.messages.map(String),
+        [],
+        'should support minWords (default)'
+      );
+    });
 
-    retext()
-        .use(readability, {
-            minWords: 0
-        })
-        .process('Honorificabilitudinitatibus.', function (err, file) {
-            t.ifError(err, 'should not fail (#8)');
+  retext()
+    .use(readability, {minWords: 0})
+    .process('Honorificabilitudinitatibus.', function (err, file) {
+      t.ifError(err, 'should not fail (#8)');
 
-            t.deepEqual(
-                file.messages.map(String),
-                ['1:1-1:29: Hard to read sentence (confidence: 4/7)'],
-                'should support `minWords` (config)'
-            );
-        });
+      t.deepEqual(
+        file.messages.map(String),
+        ['1:1-1:29: Hard to read sentence (confidence: 4/7)'],
+        'should support `minWords` (config)'
+      );
+    });
 
-    t.end();
+  t.end();
 });
